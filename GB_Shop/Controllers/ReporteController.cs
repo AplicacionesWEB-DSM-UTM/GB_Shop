5efb758259ca7ccf59c2b0193ef2ce7743188c16
Daniel Puch Ceballos <danielceballos123@hotmail.com>
2021-11-02 10:37:15 -0600

    V0

---
+++ GB_Shop/Controllers/ReporteController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReporteController : ControllerBase
+    {
+        //public string reportar(string Motivo_Denuncia, string Descrip_lugar, string Geo_ubi, string Colonia, string Foto);
+        
+    }
+}
